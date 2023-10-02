ad6f9a5c773b498bf05bf0e20f4a81b46b810f86
segunojo1 <olusegunkd2017@gmail.com>
2023-10-02 13:13:06 +0100

    vids

--- src/pages/Allvids.jsx
+++ src/pages/Allvids.jsx
@@ -3,15 +3,18 @@ import nav from "../assets/nav.png"
 
 const Allvids = () => {
   return (
-    <div>
-        <img src={nav} alt="" />
-        <div className='flex'>
+    <div className='px-10'>
+        <img src={nav} alt="" className='p-6'/>
+        <div className='flex justify-between'>
             <div>
-            <p>Hello, John Mark</p>
+            <p className='font-bold text-2xl'>Hello, John Mark</p>
             <p>Here are your recorded videos</p>
             </div>
-            <input type="text" className='p-11 bg-[#E7E7ED]' placeholder='Search for a particular vide'/>
+            <input type="text" className='p-4 bg-[#E7E7ED]' placeholder='Search for a particular vide'/>
         </div>
+        <div className='w-full bg-[#B9C2C8] h-[2px]'></div>
+
+        <p>Recent files</p>
     </div>
   )
 }
